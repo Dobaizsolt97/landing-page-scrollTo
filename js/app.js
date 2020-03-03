30f7eadc443bc6d0624a087d0e77f9ec8fa0fb6d
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-03 22:22:17 +0200

    completed requests

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// Define Global Variables
+
+// selecting all tags with the name of section
+const navbarUl = document.getElementById("navbar__list");
+const sections = document.getElementsByTagName("section");
+
+//spreadding them into an array
+const arrayOfSections = [...sections];
+//getting the id-s of those sections
+const links = arrayOfSections.map(section => section.id);
+
+// activate and deactivate a certain section of the page by removing/ adding the class
+function makeUnactive(section) {
+  const target = document.getElementById(`${section}`);
+  target.classList.remove("your-active-class");
+}
+function makeActive(section) {
+  const target = document.getElementById(`${section}`);
+  target.classList.add("your-active-class");
+}
+// adding class to highight the current section, on the navbar
+function makeLinkActive(section) {
+  const linkTarget = document.querySelector(`.${section}`);
+  linkTarget.style.background = "#333";
+  linkTarget.style.color = "white";
+}
+function makeLinkInactive(section) {
+  const linkTarget = document.querySelector(`.${section}`);
+  linkTarget.style.background = "white";
+  linkTarget.style.color = "#333";
+}
+
+// build the nav
+// generating the navbar based on the array containing the sections id's
+function populateNav(array) {
+  for (link of array) {
+    navbarUl.innerHTML += `<li><a onclick='scrollToAnchor(${link})' class='menu__link ${link}'  >${link}</a></li>`;
+  }
+}
+populateNav(links);
+
+// Scroll to anchor ID using scrollTO event
+function scrollToAnchor(link) {
+  // initializing the scrollTo distance
+  let distance = 0;
+  // getting the Oy value of our targeted section
+  const targetPozition = link.getBoundingClientRect().top;
+  // getting the current Oy position
+  const startPosition = window.pageYOffset;
+  // determining the distance to be scrolled and adjusting it considering the navbar's influence
+  distance = targetPozition + startPosition - 100;
+  window.scrollTo({
+    top: distance,
+    behavior: "smooth"
+  });
+}
+
+//
+// function that determines if a section is in viewport or not, while scrolling
+window.onscroll = () => {
+  for (link of links) {
+    let element = document.getElementById(`${link}`);
+    console.log(element);
+    let elementPosition = element.getBoundingClientRect().top;
+    console.log(elementPosition);
+    let heightElement = element.offsetHeight;
+    //if a section is in view it's top Oy value will always be greater than 0
+    // and it should extend no longet than the element's height
+    if (elementPosition >= -heightElement + 200 && elementPosition < 200) {
+      this.makeActive(link);
+      this.makeLinkActive(link);
+    } else {
+      this.makeUnactive(link);
+      this.makeLinkInactive(link);
+    }
+  }
+};
